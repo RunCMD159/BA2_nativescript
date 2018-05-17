4b061b7caafc54d94e7d4a011150952aacfe27ed
fschmuck <florian.schmuck@infonova.com>
2018-05-17 12:02:22 +0200

    [Performance] (View) - changed performance test to reset test and show list

--- app/tabs/performance/performance.component.ts
+++ app/tabs/performance/performance.component.ts
@@ -9,9 +9,8 @@ import { PerformanceService } from './performance.service';
 export class PerformanceComponent implements AfterViewChecked {
     performanceData: any = [];
     runningTime: number;
-    data = [];
 
-    private isPerformanceTestRunning: boolean = false;
+    isPerformanceTestRunning: boolean = false;
     //time in milliseconds
     private startTime: number;
     //time in milliseconds
@@ -20,9 +19,22 @@ export class PerformanceComponent implements AfterViewChecked {
 
     constructor(private performanceService: PerformanceService,
                 private changeDetector: ChangeDetectorRef) {
+        this.initPerformanceData();
+    }
+
+    private initPerformanceData() {
         for (let i = 0; i < 10000; i++) {
-            this.data.push('TestString' + i);
+            this.performanceData.push('TestString' + Math.floor((Math.random() * 10000) + 1));
         }
+    }
+
+    resetFields() {
+        this.performanceData = [];
+        this.isPerformanceTestRunning = false;
+        this.runningTime = 0;
+        this.changeDetector.markForCheck();
+        this.changeDetector.detectChanges();
+        this.initPerformanceData();
     }
 
     ngAfterViewChecked(): void {
@@ -37,24 +49,12 @@ export class PerformanceComponent implements AfterViewChecked {
     }
 
     runPerformanceTest() {
-        this.runWarmUpPhase();
-        console.log('WarmUp Phase 1 has ended');
-        this.runWarmUpPhase();
-        console.log('WarmUp Phase 2 has ended');
-
-        this.performanceData = [];
         this.isPerformanceTestRunning = true;
-        this.performanceService.runPerformanceTest(this.data).subscribe((perfData) => {
+        console.log(this.performanceData.length);
+        this.performanceService.runPerformanceTest(this.performanceData).subscribe((perfData) => {
             console.log('Performance Test Started');
             this.startTime = new Date().getTime();
             this.performanceData = perfData;
-        })
-    }
-
-    runWarmUpPhase() {
-        this.performanceService.runWarmUpPhase().subscribe((warmUpData) => {
-            console.log('WarmUp Phase Started');
-            this.performanceData = warmUpData;
         })
     }
 }
--- app/tabs/performance/performance.service.ts
+++ app/tabs/performance/performance.service.ts
@@ -1,27 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class PerformanceService {
 
-    constructor(private http: HttpClient) {
+    constructor() {
     }
 
-
-    public runWarmUpPhase(): Observable<any> {
-        let data = ['WARM UP'];
-        return Observable.create((observer) => {
-            observer.next(data)
-        });
-        // return this.http.get("")
-    }
 
     public runPerformanceTest(data): Observable<any> {
-
         return Observable.create((observer) => {
             observer.next(data)
         });
-        // return this.http.get("")
     }
 }
